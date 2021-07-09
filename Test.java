2bca61f52c70c65fb6aa621e1c4eb2dca8b69d7b
leoruan <ruanchengui@163.com>
2021-07-09 18:13:44 +0800

    update annotation

--- Test.java
+++ Test.java
@@ -31,6 +31,9 @@ public class Test
         new Test().testCasesFindPairs();
     }
 
+    /**
+     * 测试
+     */
     public void testCasesFindPairs(){
         testCase1();
         testCase2();
@@ -38,14 +41,35 @@ public class Test
         testCase4();
     }
 
+    /**
+     * 针对case1的测试
+     */
     public void testCase1(){
+        /**
+         * 输入目标合值
+         */
         int target = 10;
+        /**
+         * 源整数数组
+         */
         Integer[] listOfIntegers = new Integer[]{3,1,7,5,6};
+        /**
+         * 预期：输出结果对应的数字，按照顺序排列
+         */
         String output = "37";
+        /**
+         * 预期：输出匹配结果的个数
+         */
         int matchSize = 1;
+        /**
+         * 测试
+         */
         testCase0(target,listOfIntegers,matchSize,output);
     }
 
+    /**
+     * 针对case2的测试
+     */
     public void testCase2(){
         int target = 10;
         Integer[] listOfIntegers = new Integer[]{3, 7, 5, 5, 5, 1, 6};
@@ -54,6 +78,9 @@ public class Test
         testCase0(target,listOfIntegers,matchSize,output);
     }
 
+    /**
+     * 针对case3的测试
+     */
     public void testCase3(){
         int target = 10;
         Integer[] listOfIntegers = new Integer[]{3, 3, 7, 7, 5, 1, 6, 5};
@@ -62,6 +89,9 @@ public class Test
         testCase0(target,listOfIntegers,matchSize,output);
     }
 
+    /**
+     * 针对case4的测试
+     */
     public void testCase4(){
         int target = 10;
         Integer[] listOfIntegers = new Integer[]{3, 3, 4, 1, 8};
@@ -70,6 +100,13 @@ public class Test
         testCase0(target,listOfIntegers,matchSize,output);
     }
 
+    /**
+     * 测试，如果测试不通过则抛出运行时异常（自定义）
+     * @param target  目标合值
+     * @param listOfIntegers 源整数数组
+     * @param matchedSize 预期匹配个数
+     * @param output 预期输出字符串
+     */
     public void testCase0(int target,Integer[] listOfIntegers,int matchedSize,String output){
         List<Integer[]> ret = findPairs(target,listOfIntegers);
         if(ret.size()!=matchedSize)
@@ -98,26 +135,36 @@ public class Test
     }
 
     /**
-     * 如果没有值则返回空
-     * @param targetIntger
-     * @param listOfIntegers
-     * @return
+     * pick pairs of integers which are summed up to the target integer
+     * @param targetIntger  target integer
+     * @param listOfIntegers  source integers
+     * @return java.util.List  Integer[]
      */
     public List<Integer[]> findPairs(int targetIntger, Integer[] listOfIntegers)
     {
         List<Integer[]> ret = new ArrayList<Integer[]>();
+        /**
+         * 用于标记哪一些位置上的整数已经被使用过
+         */
         List<Integer> usedIndex = new ArrayList<>();
-
 
         for(int i=0;i<listOfIntegers.length-1;i++)
         {
             for(int j=1;j<listOfIntegers.length;j++)
             {
+                /**
+                 * 满足两者相加等于目标整数 & 不可以自己加自己 & 使用过的位置不再重复使用
+                 */
                if(listOfIntegers[i] + listOfIntegers[j] == targetIntger && i!=j && (!usedIndex.contains(i) && !usedIndex.contains(j)))
                {
+                   /**
+                    * 满足加入返回结果集合中
+                    */
                    ret.add(new Integer[]{listOfIntegers[i],listOfIntegers[j]});
                    System.out.println(listOfIntegers[i]+" - " + listOfIntegers[j]);
-
+                   /**
+                    * 标记指定的i 、j位置为使用过的位置，等价于从原集合中移除这两项
+                    */
                    usedIndex.add(i);
                    usedIndex.add(j);
                }
